d73e2c36f5a997d9c98795aefdb0f1ddf2f801e3
OksanaMinchuk <oks.m89@mail.ru>
2020-11-27 16:54:23 +0300

    feature_commit_1

--- TestGitTask/src/main/java/AppMain.java
+++ TestGitTask/src/main/java/AppMain.java
@@ -2,5 +2,6 @@ public class AppMain {
     public static void main(String[] args) {
         System.out.println("hello_master_1");
         System.out.println("hello_master_2");
+        System.out.println("feature_1");
     }
 }
