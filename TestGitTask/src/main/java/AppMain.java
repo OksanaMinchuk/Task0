3684cb0147ce061da6215f5e5583e773848ed6bb
OksanaMinchuk <oks.m89@mail.ru>
2020-11-27 17:19:34 +0300

    master_commit_6

--- TestGitTask/src/main/java/AppMain.java
+++ TestGitTask/src/main/java/AppMain.java
@@ -1,5 +1,6 @@
 public class AppMain {
     public static void main(String[] args) {
-        System.out.println("hello_master");
+        System.out.println("hello_master_1");
+        System.out.println("hello_master_2");
     }
 }
